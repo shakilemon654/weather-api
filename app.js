5e20f3a2e23dfbe070dcaabb549798a6c7f13359
shakilemon654 <shakilemon654@gmail.com>
2020-09-15 12:34:59 +0300

    api call url bug fixed

--- app.js
+++ app.js
@@ -4,19 +4,19 @@
     })
 
     const getWeatherData = city => {
-        const apiBase = "http://api.openweathermap.org/data/2.5/forecast";
+        const apiBase = "https://api.openweathermap.org/data/2.5/weather";
         const apiKey = "1468198d3a968545675628650807cc69";
-        const url = `${apiBase}?q=${city}&units=metric&APPID=${apiKey}`;
+        const url = `${apiBase}?q=${city}&appid=${apiKey}`;
         fetch(url)
         .then(response => response.json())
         .then(data => updateWeatherData(data))   
     }
 
     const updateWeatherData = data => {
-        document.getElementById("icon").setAttribute("src", `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`)
-        document.getElementById("update-city-name").innerHTML = data.city.name;
-        document.getElementById("update-temperature").innerText = data.list[0].main.temp;
-        document.getElementById("update-weather-status").innerText = data.list[0].weather[0].main;
+        document.getElementById("icon").setAttribute("src", `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`)
+        document.getElementById("update-city-name").innerHTML = data.name;
+        document.getElementById("update-temperature").innerText = Math.round(data.main.temp - 273.15);
+        document.getElementById("update-weather-status").innerText = data.weather[0].main;
         document.getElementById("input-city").value = "";
     }
 
